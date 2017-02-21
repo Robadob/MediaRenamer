a48e53777d8be929bf00baff6db991842318ac1d
Robert Chisholm <robadob@robadob.org>
2017-02-21 18:25:16 +0000

    Added support for runtime args, silent, recurse, inplace, src, dest

--- rename.py
+++ rename.py
@@ -3,12 +3,13 @@ import os.path;
 import re;
 import shutil;
 import pickle;
+import sys;
 
 #Cleans a string of chars which cannot be in a windows file path
 def cleanString( inp ):
     inp = str(inp).replace('\\',', ').replace('/',', ').replace(':',', ').replace('*','').replace('?','').replace('"','').replace('<','(').replace('>',')').replace('|','');
     return inp;
-
+#Reads a plaintext dictionary from the file specified by arg
 def readDictionary ( inp ):
     rtn = {};
     for tln in open(inp):
@@ -19,10 +20,87 @@ def readDictionary ( inp ):
         else:
             print("Err unable to parse replacement: %s" % (tln));
     return rtn;
+#Attempts to turn arg showName into a valid show name recognised by tvdb
+#Returns emptystring on failure
+def findShow ( showName, isSilent ):
+    showResults = tvdb.search(showName, 'en')
+    if len(showResults)!=1:
+        while len(showResults)!=1:
+            #If silent, exit with failure
+            if isSilent:
+                return "";
+            #If no results, ask for new term and rerun search
+            elif len(showResults) == 0:
+                print("'%s' does not resolve to any results, please enter an alternate search." %(showName))
+                showName = input("Search term: ")
+                showResults = tvdb.search(showName, 'en')
+            #If multiple results, prompt the user to select the desired
+            elif len(showResults) > 1:#Could also check result 1 isn't identical to cleaned show name
+                print("'%s' has multiple results, please select desired result." %(showName))
+                for i in range(0,len(showResults)):
+                    print("%d: %s" % (i, showResults[i].SeriesName))
+                print("x: Perform new search")
+                while len(showResults) > 1:
+                    option = input("Enter desired option: ").strip().lower();
+                    if option=='x':
+                        showResults=[]
+                    elif option.isdigit() and int(option) < len(showResults):
+                        showResults=[showResults[int(option)]]
+    return showResults[0];
     
-videoRoot = 'D:\Test'
-videoTypes = ['.mp4', '.avi', '.mkv', '.wmv']
-files = []
+#Config
+runSilent = False;
+runRecursive = False;
+runInplace = False;
+videoRoot = ".";
+videoDest = "";
+videoTypes = [".mp4", ".avi", ".mkv", ".wmv"];
+files = [];
+
+def printHelp():
+    print("Usage:");
+    print("rename.py [args] <sourcePath>");
+    print("Args:");
+    print("-silent, -s: Suppress output");
+    print("-recurse, -r: Recursively find files in source path");
+    print("-inplace, -i: Rename files inplace, not compatible with -dest or -d");
+    print("-dest <destPath>, -d <destPath>: Specify a destination path (else source will be used)");
+    exit();  
+
+#Process Args
+if len(sys.argv)>1:
+    for i in range(1,len(sys.argv)-1):
+        if sys.argv[i]=="-help" or sys.argv[i]=="-?":
+            printHelp();
+        elif sys.argv[i]=="-silent" or sys.argv[i]=="-s":
+            runSilent = True;
+        elif sys.argv[i]=="-recurse" or sys.argv[i]=="-r":
+            runRecursive=True;
+        elif sys.argv[i]=="-inplace" or sys.argv[i]=="-i":
+            runInplace=True;
+        elif sys.argv[i]=="-dest" or sys.argv[i]=="-d":
+            i=i+1;
+            if i < (len(sys.argv)-1):
+                videoDest=sys.argv[i];
+            else:
+                print("-dest, -d args require a trailing arg");
+                printHelp();
+        else:
+            print("Arg '%s' was not recognised." % (ys.argv[i]));
+            printHelp();
+else:
+    printHelp();
+
+videoRoot = sys.argv[len(sys.argv)-1];
+    
+    
+#Block bad args
+if runInplace and videoDest!="":
+    print("-inplace,-i and -dest,-d args are incompatible");
+    printHelp();
+  
+if videoDest=="":
+    videoDest=videoRoot;
 
 #Regexes
 showRegex = re.compile('^(.+?([. ][0-9]{4})?)[ .]-?[ .]?(s(eason[. ]?)?)?([0-9]{1,2}?)[ x.]?(e(pisode[. ]?)?)?([0-9]{2})', re.IGNORECASE);
@@ -35,8 +113,12 @@ for root, directories, filenames in os.walk(videoRoot):
             if filename.lower().endswith(type):
                 files.append(os.path.join(root,filename));
                 continue;
-       
-print("%d video files found for processing." % (len(files)));
+    #If not recursive, skip others
+    if not runRecursive:
+        break;
+
+if not(runSilent):       
+    print("%d video files found for processing." % (len(files)));
 
 tvdb = api.TVDB('A0857036BEACEE1A');
 
@@ -53,37 +135,18 @@ for file in files:
     type = typeRegex.search(filename).group(0)
     show = showRegex.match(filename)
     if not(show):
-        print("Cannot detect show details in file: %s" % (filename))
+        if not(runSilent): 
+            print("Cannot detect show details in file: %s" % (filename))
     else:
         showName = show.group(1).replace('.',' ').strip().lower()
         showSeasonNo = int(show.group(5))
         showEpisodeNo = int(show.group(8))
         #print("%s - s%se%s - null" % (showName, showSeasonNo, showEpisodeNo))
         if not(showName in namePairs):
-            showResults = tvdb.search(showName, 'en')
-            if len(showResults)!=1:
-                oldShowName = showName
-                while len(showResults)!=1:
-                    #If no results, ask for new term and rerun search
-                    if len(showResults) == 0:
-                        print("'%s' does not resolve to any results, please enter an alternate search." %(showName))
-                        showName = input("Search term: ")
-                        showResults = tvdb.search(showName, 'en')
-                    #If multiple results, prompt the user to select the desired
-                    elif len(showResults) > 1:#Could also check result 1 isn't identical to cleaned show name
-                        print("'%s' has multiple results, please select desired result." %(showName))
-                        for i in range(0,len(showResults)):
-                            print("%d: %s" % (i, showResults[i].SeriesName))
-                        print("x: Perform new search")
-                        while len(showResults) > 1:
-                            option = input("Enter desired option: ").strip().lower();
-                            if option=='x':
-                                showResults=[]
-                            elif option.isdigit() and int(option) < len(showResults):
-                                showResults=[showResults[int(option)]]
-                #Add show to the dictionary of namePairs and continue as though it was there to begin with
-                showName=oldShowName
-            namePairs[showName]=showResults[0];
+            showNameConfirmed = findShow(showName,runSilent);
+            if showNameConfirmed=="":
+                continue;
+            namePairs[showName]=showNameConfirmed;
         else:
             #Update show stored in namePairs
             namePairs[showName]=tvdb.search(namePairs[showName].SeriesName, 'en')[0];
@@ -96,16 +159,18 @@ for file in files:
         showSeason = show[showSeasonNo]
         showEpisode = showSeason[showEpisodeNo]
         #Create new file name ~/Show Name/Show Name - sXXeYY - Episode Name.type
-        newFilePath = videoRoot
+        #runInplace?videoRoot:videoDest;
+        newFilePath = videoRoot if runInplace else videoDest;
         #Ensure the root ends with a slash
-        if not(videoRoot.endswith(os.path.sep)):
+        if not(newFilePath.endswith(os.path.sep)):
             newFilePath += os.path.sep
-        #Append the show name as a directory
-        newFilePath += cleanString(showName)
-        newFilePath += os.path.sep
-        #Attempt to create that directory
-        if not os.path.exists(newFilePath):
-            os.mkdir(newFilePath)
+        if not runInplace:
+            #Append the show name as a directory
+            newFilePath += cleanString(showName)
+            newFilePath += os.path.sep
+            #Attempt to create that directory
+            if not os.path.exists(newFilePath):
+                os.mkdir(newFilePath)
         #Append the show name as part of the file name
         newFilePath += cleanString(showName)
         newFilePath += ' - '
@@ -120,7 +185,7 @@ for file in files:
         newFilePath += cleanString(showEpisode.EpisodeName)
         #Append file type
         newFilePath += type
-        if file != newFilePath:
+        if file != newFilePath and not(runSilent):
             print(file.encode("utf-8"))
             print(newFilePath.encode("utf-8"))
         #Move file
